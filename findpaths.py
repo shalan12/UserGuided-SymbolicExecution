c8fc4c135d09d0e24cfc1b964d6f1645469856b9
shalan12 <syed_shalan_naqvi@hotmail.com>
2015-06-21 21:45:47 +0500

    mostly done

--- findpaths.py
+++ findpaths.py
@@ -17,20 +17,60 @@ class BasicBlockNode:
 		self.Then = null
 		self.Else = null
 
-	def getPaths(self,pathConditions,symbolTables,currTable,currCondition):
+	def getPaths(self,currTable,currCondition):
 		for instruction in self.instructions:
 			temp = getInstructionType(instruction)
 			if (temp[0] == ASSIGNMENT_INSTRUCTION):
-				updateCurrSymbolTable(self,temp[1],temp[2],currTable)
+				updateCurrSymbolTable(temp,currTable)
 			elif (temp[0] == BRANCH_INSTRUCTION):
+				pathSymbs = []
 				if(self.Then != null):
-					getPaths(self.Then, pathConditions,symbolTables, currTable, currCondition + "and" + temp[1])
+					pathSymbs = pathSymbs + getPaths(self.Then, currTable, currCondition + "and" + temp[1])
 				if(self.Else != null):
-					getPaths(self.Else,pathConditions,symbolTables,currTable,currCondition + "and" + temp[1])
+					pathSymbs = pathSymbs + getPaths(self.Else,currTable,currCondition + "and not(" + temp[1]+")")
+				if(self.Next != null):
+					pathSymbs2  = getPaths(self.Next,currTable,[])
+					retPathSymbs = extendPaths(pathSymbs,pathSymbs2)
+					return (retPathSymbs)
+				else:
+					return pathSymbs
 
-		return (pathConditions+currCondition,symbolTables+currTable)
+		return (currCondition,currTable)
+
+	def extendPathSymbs(pathSymbs,pathSymbs2):
+		for (pathSymb) in pathSymbs:
+			for (pathSymb2) in pathSymbs2:
+				extendPathSymb(pathSymb,pathSymb2)
+
+	def extendPathSymb(pathSymb,pathSymb2):
+		symbolTable2 = pathSymb2[1]
+		symbolTable = pathSymb[1]
+		retPathSymbs = []
+		for (key,val) in symbolTable2:
+			if(val[1] == 0 and symbolTable.get(key,('blah',0))[1] == 1):
+				varss = getVarsInExp(val[0])
+				for var in varss:
+					if(symbolTable2.get(var,0) == 0 and symbolTable.get(var,0) == 1):
+						updateCurrSymbolTable(var,symbolTable[var],False,[],symbolTable2)
+						retPathSymbs = retPathSymbs + (pathSymb[0],symbolTable2)
 
 
+
+	def updateCurrSymbolTable(lhs,rhs,isConstant,vars,currTable):
+		if(isConstant):
+			currTable[lhs] = (rhs,1)
+		else:
+			known = True
+			for var in vars:
+				if(not currTable.get(var,False)):
+					known = False
+					break
+			currTable[lhs] = (expandVars(rhs,symbolTable),known)
+
+	def expandVars(expression,symbolTable):
+		#replace variables with their values in terms of consts and function inputs
+	def getVarsinExp(expression):
+		# returns all the variables in the expression
 
 def getInstructionType(instruction):
 	#returns array containing type of instruction, plus some other data	
@@ -40,6 +80,6 @@ def isAssignmentInstruction(instruction):
 def findPaths(function,inputs):
 	symbols = {}
 	for input in inputs:
-		symbols[input] = input
+		symbols[input] = (input,1)
 	basicblocks = getBasicBlocks(function)
 
