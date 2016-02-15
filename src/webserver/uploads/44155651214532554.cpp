f8fddfaa7ef2ebd5eb5d541462039fb1607b836d
Habiba-Farrukh <h.farrukh13@gmail.com>
2016-02-15 18:37:35 +0500

    Interface changed

---
+++ src/webserver/uploads/44155651214532554.cpp
@@ -0,0 +1,35 @@
+#include "../../errors.h"
+#include <stdlib.h>     /* abs */
+int notmain2(int c, int d);
+void notmain(int a, int b)
+{
+	if (a > b)
+	{
+		if(a+1 > b)
+		{
+			b = notmain2(a+1,b+2);
+			b = b + abs(a);
+		}
+		else
+		{
+			b = b + 1;
+		}
+	}
+	else
+	{
+		int x = 1;
+		while(x > 0)
+		{
+			a++;
+			x--;
+		}
+		b = b + 2;
+	}
+}
+int notmain2(int c, int d)
+{
+	if(c > d)
+		return c+d;
+	else
+		return c-d;
+}
