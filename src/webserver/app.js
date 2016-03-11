72cfb20592fbd0871b00e9476e5a4b8f0743dff4
shalan12 <syed_shalan_naqvi@hotmail.com>
2016-03-11 23:58:42 +0500

    temp fix added for cookie issue

--- src/webserver/app.js
+++ src/webserver/app.js
@@ -25,6 +25,8 @@ var exec = require('child_process').exec,
 var fs = require('fs');
 var util = require('util');
 var lodash = require('lodash');
+var Chance = require('chance');
+var chance = new Chance();
 // var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
 // var log_stdout = process.stdout;
 
@@ -44,7 +46,6 @@ var lodash = require('lodash');
 // // });
 
 
-var map = []; // contains sessionid->filename,lastPinged
 var toSend = [];
 var MSG_TYPE_EXCLUDENODE = 100;
 var MSG_TYPE_EXPANDNODE = 200;
@@ -52,32 +53,7 @@ var MSG_TYPE_FUNCNAMES = 300;
 /////////////////////////////////////
 ////////////UTILS///////////////////
 ///////////////////////////////////
-function isEmptyObject(obj) 
-{
-  return !Object.keys(obj).length;
-}
-function getRandomInt()
-{
-  var randomNumber;
-  var found;
- do 
- {
-  found = true;
-  randomNumber = Math.random();
-  randomNumber = randomNumber.toString().substring(2,randomNumber.length);
 
-  for (var key in map) 
-  {
-    if (map.hasOwnProperty(key) &&  randomNumber == key) 
-    {
-      found = false
-      break
-    }
-  }
-
-  }while (!found);
-  return randomNumber;
-}
 function isPingQuery(query)
 {
   return (query['isPing'].valueOf() != 'false');
@@ -138,13 +114,7 @@ client.on('data',function(data)
 {
     console.log("recieved from symbolicexecutor " + data);
     data = JSON.parse(data);
-    if(data.fin  === "1")
-    {
-      var idx = map.indexOf(data.sessionid);
-      if(idx != -1) map.splice(idx,1);
-      client.end("FIN");
-      //delete file
-    }
+    if(data.fin  === "1") client.end("FIN");
     else
     {
       if(data["type"] == MSG_TYPE_EXPANDNODE)
@@ -179,10 +149,6 @@ client.on('data',function(data)
     }  
    
 });
-// client.setTimeout(timeout,function()
-// {
-
-// });
 
 app.configure(function(){
   app.set('port', process.env.PORT || 80);
@@ -192,26 +158,10 @@ app.configure(function(){
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.cookieParser());
+  app.use(express.session({secret: 'i12kroejfpajwke-012joepegvn2je0j2pwkps'}));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(function (req, res, next) {
-  // check if client sent cookie
-  var cookie = req.cookies.sessionid;
-  if (cookie === undefined)
-  {
-    // no: set a new cookie
-    res.cookie('sessionid',getRandomInt(), { maxAge: 900000, httpOnly: true });
-    console.log('cookie created successfully');
-  } 
-  else
-  {
-    // yes, cookie was already present 
-    console.log('cookie exists', cookie);
-  } 
-  next(); // <-- important!
-  });
-  
   app.use(require('stylus').middleware(__dirname + '/public'));
   app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -226,14 +176,6 @@ app.get('/main',function(req,res){
   });
 });
 app.get('/refresh',function(req,res){
-  console.log("here");
-  var filename = map[req.cookies.sessionid].slice(0,-3);
-  console.log(filename);
-  var idx = map.indexOf(filename);
-  if(idx > -1)
-  {
-    map.splice(idx,1);
-  }
   res.clearCookie(req.cookies.sessionid);
   exec('rm /uploads/' + filename+'.bc',  function(error, stdout, stderr){
       if(error) console.log(error);
@@ -264,132 +206,119 @@ app.get('/contact', function(req, res){
 app.get('/sample',function(req,res)
 {
   var path = __dirname + "/samples/" + req.query["fileID"];
-  var filename = req.cookies.sessionid;
+  var filename = chance.string({length:10, pool:"abcdefghijklmnopqrstuvwxyz"});
   var newPath = __dirname + "/uploads/" + filename;
-
-  console.log("id = " + req.cookies.sessionid);
-  map[req.cookies.sessionid] = makeBitCodeFile(path, newPath, req.cookies.sessionid);
-  map[req.cookies.sessionid].then(function()
+  makeBitCodeFile(path, newPath, filename).then(function(fileId)
   {
+    req.session.file = fileId;
     res.sendfile(path);
-  })
+  });
 });
 app.post('/upload',function(req,res){
   var extension = '.cpp'; 
   var filename = req.cookies.sessionid;
   var newPath = __dirname + "/uploads/" + filename; //__dirname is a global, containing the current dir 
-  map[req.cookies.sessionid] = makeBitCodeFile(req.files.SelectedFile.path, newPath, req.cookies.sessionid);
+  if (req.session.file == undefined)
+    req.session.file = makeBitCodeFile(req.files.SelectedFile.path, newPath, req.cookies.sessionid);
   res.redirect('back'); // return to the previous page
 });
 
 app.get('/next',function(req,res){
-  map[req.cookies.sessionid].then(function(fileId)
+  var fileId = req.session.file;
+  query = req.query;
+  if(!isPingQuery(query))
   {
-    query = req.query;
-    if(!isPingQuery(query))
-    {
-      toSendToExecutor = {};
-      toSendToExecutor["val"] = {}
-      toSendToExecutor["val"]["isBFS"] = (typeof query["isBFS"] === 'undefined' || query["isBFS"] === '') ? 0:query["isBFS"];
-      toSendToExecutor["val"]["branch"] = (typeof query["branch"] === 'undefined' || query["branch"] === '' ) ? 0:query["branch"];
-      toSendToExecutor["val"]["steps"] = (typeof query["steps"] === 'undefined' || query["steps"] === '') ? 1:query["steps"];
-      toSendToExecutor["val"]["prevId"] = (typeof query["prevId"] === 'undefined' || query["prevId"] === '') ? -1:query["prevId"];
-      toSendToExecutor["id"] = fileId;
-      console.log("Sending to Executor : " );
-      console.log(toSendToExecutor);
-      client.write(JSON.stringify(toSendToExecutor));
-    }
-    //console.log("sending to user : " + JSON.stringify(toSend[fileId]));
-    if(toSend[fileId])
-    {
-      //sendCopy = lodash.cloneDeep(toSend[fileId]);
-      console.log("sending to client " + JSON.stringify(toSend[fileId]));
-      res.send(toSend[fileId]);
-      toSend[fileId] = null;
-    }
-    else
-    {
-      res.send({"updated":false,"completed":false});
-    }
- });
-  
+    toSendToExecutor = {};
+    toSendToExecutor["val"] = {}
+    toSendToExecutor["val"]["isBFS"] = (typeof query["isBFS"] === 'undefined' || query["isBFS"] === '') ? 0:query["isBFS"];
+    toSendToExecutor["val"]["branch"] = (typeof query["branch"] === 'undefined' || query["branch"] === '' ) ? 0:query["branch"];
+    toSendToExecutor["val"]["steps"] = (typeof query["steps"] === 'undefined' || query["steps"] === '') ? 1:query["steps"];
+    toSendToExecutor["val"]["prevId"] = (typeof query["prevId"] === 'undefined' || query["prevId"] === '') ? -1:query["prevId"];
+    toSendToExecutor["id"] = fileId;
+    console.log("Sending to Executor : " );
+    console.log(toSendToExecutor);
+    client.write(JSON.stringify(toSendToExecutor));
+  }
+  //console.log("sending to user : " + JSON.stringify(toSend[fileId]));
+  if(toSend[fileId])
+  {
+    //sendCopy = lodash.cloneDeep(toSend[fileId]);
+    console.log("sending to client " + JSON.stringify(toSend[fileId]));
+    res.send(toSend[fileId]);
+    toSend[fileId] = null;
+  }
+  else
+  {
+    res.send({"updated":false,"completed":false});
+  }  
 });
 app.get('/constraints',function(req,res){
-  map[req.cookies.sessionid].then(function(fileId){
-    query = req.query;
-    console.log(query);
-    if(!isPingQuery(query))
-    {
-      var zipped = lodash.zip(query["inputConstraints"].split(","), query["expectedOutput"].split(",")); 
-      var pairs = lodash.map(zipped, function(x){
-        return {"constraints":x[0],"returnValue":x[1]};
-      });
-      client.write(JSON.stringify({"id":fileId, "val":{"nodeid": query["nodeid"], "pairs": pairs}}));
-    }
-    else if (toSend[fileId])
-    {
-        console.log("sending" + JSON.stringify(toSend[fileId]))
-        res.send(toSend[fileId]);
-        toSend[fileId] = null;
-        return;
-    }
-    res.send({}); 
-  });
-    
-  
- 
+  var fileId = req.session.file;
+  query = req.query;
+  console.log(query);
+  if(!isPingQuery(query))
+  {
+    var zipped = lodash.zip(query["inputConstraints"].split(","), query["expectedOutput"].split(",")); 
+    var pairs = lodash.map(zipped, function(x){
+      return {"constraints":x[0],"returnValue":x[1]};
+    });
+    client.write(JSON.stringify({"id":fileId, "val":{"nodeid": query["nodeid"], "pairs": pairs}}));
+  }
+  else if (toSend[fileId])
+  {
+      console.log("sending" + JSON.stringify(toSend[fileId]))
+      res.send(toSend[fileId]);
+      toSend[fileId] = null;
+      return;
+  }
+  res.send({}); 
 
 });
 app.get('/metadata', function(req,res)
 {
-  map[req.cookies.sessionid].then(function(fileId)
+  var fileId = req.session.file;
+  console.log(toSend[fileId]);
+  query = req.query;
+  if (!isPingQuery(query))
   {
-    console.log(toSend[fileId]);
-    query = req.query;
-    if (!isPingQuery(query))
-    {
-      toSendToExecutor = {"id" : fileId, "type": MSG_TYPE_FUNCNAMES};
-      console.log("sending " + JSON.stringify(toSendToExecutor));
-      client.write(JSON.stringify(toSendToExecutor));
-    }
-    if (toSend[fileId])
-    {
-      res.send(toSend[fileId]);
-      toSend[fileId] = null;
-    }
-    else
-    {
-      res.send({});
-    }
-  });
-
+    toSendToExecutor = {"id" : fileId, "type": MSG_TYPE_FUNCNAMES};
+    console.log("sending " + JSON.stringify(toSendToExecutor));
+    client.write(JSON.stringify(toSendToExecutor));
+  }
+  if (toSend[fileId])
+  {
+    res.send(toSend[fileId]);
+    toSend[fileId] = null;
+  }
+  else
+  {
+    res.send({});
+  }
   
 });
 app.get('/exclude', function(req,res){
-  map[req.cookies.sessionid].then(function(fileId)
+  var fileId = req.session.file;
+  query = req.query;
+  if(!isPingQuery(query))
   {
-    query = req.query;
-    if(!isPingQuery(query))
+    if(query["lineno"]) 
+      toSendToExecutor = {"id":fileId, "val": {"exclude":query["lineno"], "isNode":"0"}};
+    else
     {
-      if(query["lineno"]) 
-        toSendToExecutor = {"id":fileId, "val": {"exclude":query["lineno"], "isNode":"0"}};
-      else
-      {
-        toSendToExecutor = {"id":fileId, "val": {"exclude":query["nodeid"], "isNode":"1"}};
-        //temp - because ubaid not replying in this case
-      }
-      res.send({});
-      client.write(JSON.stringify(toSendToExecutor));
+      toSendToExecutor = {"id":fileId, "val": {"exclude":query["nodeid"], "isNode":"1"}};
+      //temp - because ubaid not replying in this case
+    }
+    res.send({});
+    client.write(JSON.stringify(toSendToExecutor));
 
-    }
-    if(toSend[fileId])
-    {
-        res.send(toSend[fileId]);
-        console.log("sending" + JSON.stringify(toSend[fileId]))
-        toSend[fileId] = null;
-    }
-    else res.send({});  
-  });
+  }
+  if(toSend[fileId])
+  {
+      res.send(toSend[fileId]);
+      console.log("sending" + JSON.stringify(toSend[fileId]))
+      toSend[fileId] = null;
+  }
+  else res.send({});  
   
 });
 server = http.createServer(app).listen(app.get('port'), function(){
